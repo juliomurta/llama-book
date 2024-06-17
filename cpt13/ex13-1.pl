997445490956d8d333f06202b33c9a17a388d2fc
Julio Cesar Souza Murta <souzamurtaj@gmail.com>
2024-06-17 09:12:56 -0300

    ex 1 cpt 13

---
+++ cpt13/ex13-1.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+
+$dir = @ARGV[0];
+if( defined($dir)) {
+    chdir $dir or die "\nCannot access $dir: $!";
+} else {
+    chdir or die "\nCannot access $ENV{HOME}: $!";
+}
+
+foreach $file (glob '*'){
+    print "\n$file";
+}
